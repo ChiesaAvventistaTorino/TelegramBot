75d2ce4849e689168bdf18eae40942911a5d2052
ChiesaAvventistaTorino <avventisti.torino@gmail.com>
2025-02-20 13:33:41 +0100

    Update script_telegram.py

--- script_telegram.py
+++ script_telegram.py
@@ -15,7 +15,7 @@ CHANNEL_ID = os.getenv("CHANNEL_ID")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 POST_HOUR = 13  # Set the hour (24-hour format)
-POST_MINUTE = 25  # Set the minute
+POST_MINUTE = 40  # Set the minute
 
 # Initialize bot
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
