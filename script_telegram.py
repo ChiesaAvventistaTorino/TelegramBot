58babc91cdb5120bae2119abce6280fa99fe0933
ChiesaAvventistaTorino <avventisti.torino@gmail.com>
2025-02-21 09:02:36 +0100

    Updated bot with week support

--- script_telegram.py
+++ script_telegram.py
@@ -88,7 +88,7 @@ async def telegram_loop():
 
         logger.info(f"Orario italiano attuale: {now_italy.strftime('%A %H:%M:%S')}")  # Formato 24 ore
 
-        if now_italy.weekday() == 5 and now_italy.hour == POST_HOUR and now_italy.minute == POST_MINUTE and now_italy.second == 0:
+        if now_italy.weekday() == 4 and now_italy.hour == POST_HOUR and now_italy.minute == POST_MINUTE and now_italy.second == 0:
             await post_to_telegram()
             await asyncio.sleep(60)  # Evita duplicati nello stesso minuto
         await asyncio.sleep(1)
